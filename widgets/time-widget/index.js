8b8112e8f66bda0b996999e15e03139fab1e07e6
Emil Johansson <emiljohansson.se@gmail.com>
2014-11-11 14:18:55 -0600

    fix hours greater than 24

--- widgets/time-widget/index.js
+++ widgets/time-widget/index.js
@@ -23,10 +23,16 @@
         }
         return {
             minutes: minutes < 10 ? "0"+minutes : minutes,
-            hours: hours < 10 ? "0"+hours : hours,
+            hours: fixHours(hours),
             title: (this.getAttribute('title') || 'Local')
         };
     };
+
+    function fixHours(hours) {
+        if (hours < 10) {return "0"+hours;}
+        if (hours >= 24) {return hours - 24;}
+        return hours;
+    }
 
     document.registerElement('time-widget', {
         prototype: element,
